3f44ca5f4f7bce5dbba8f41935daf6888f13069a
Krupal Patel <krupal9102001@gmail.com>
2024-04-01 13:56:05 +0530

    Fetching API Completed

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ console.log("This is tutorial");
 
 // Utility function 
 // 1. utility function to get DOM element from string
-   function getElementFromString(string){
+function getElementFromString(string) {
     let div = document.createElement('div');
-    div.innerHTML=string;
-    return div.firstElementChild; 
-   }
+    div.innerHTML = string;
+    return div.firstElementChild;
+}
 
 
 // hide the parameters box initially
@@ -48,18 +48,18 @@ addParam.addEventListener('click', () => {
                          <button class="btn btn-primary m-2 deleteParam" >-</button>
                     </div>`;
 
-                    addedParamCounts ++;
-   
-     // Convert the element string to DOM node
-     let paramElement = getElementFromString(string);
-     params.appendChild(paramElement); 
+    addedParamCounts++;
 
-     // Add a event listener to remove the parameters on clicking button 
+    // Convert the element string to DOM node
+    let paramElement = getElementFromString(string);
+    params.appendChild(paramElement);
+
+    // Add a event listener to remove the parameters on clicking button 
     let deleteParam = document.getElementsByClassName('deleteParam')
-    for (item of deleteParam){
-        item.addEventListener("click", (e)=>    {
+    for (item of deleteParam) {
+        item.addEventListener("click", (e) => {
             e.target.parentElement.remove();
-            e.alert("Are you Sure to delete parameters?"); // Error /////////////////////////////////////////////////////
+            // e.alert("Are you Sure to delete parameters?"); // Error /////////////////////////////////////////////////////
         })
     }
 
@@ -67,7 +67,66 @@ addParam.addEventListener('click', () => {
 
 // If the user click on the "Submit" button in form
 let submitBtn = document.getElementById('submitBtn');
-submitBtn.addEventListener('click', ()=>{
-    
+submitBtn.addEventListener('click', () => {
+    // Show please wait in the response box to request patience from the user
+    document.getElementById("responseJsonText").value = "Please Wait till the response  is generated...";
+
+    // Fetch all the values user has entered
+    let url = document.getElementById('urlField').value;
+    let requestType = document.querySelector("input[name = 'requestType']:checked").value;
+    let contentType = document.querySelector("input[name = 'contentType']:checked").value;
+
+  
+    // If user has used params option instead of json, collect all parameters in an object
+    if (contentType == 'params') {
+        data = {};
+        for (i = 0; i < addedParamCounts + 1; i++) {
+            if (document.getElementById('parameterKey' + (i + 1)) != undefined) {
+                let key = document.getElementById('parameterKey' + (i + 1)).value;
+                let value = document.getElementById('parameterValue' + (i + 1)).value;
+                data[key] = value;
+            }
+            data = JSON.stringify(data);
+
+        }
+    }
+    else { 
+        data = document.getElementById('requestJsonText').value
+        }
+      // Log all the values in the console
+    console.log("URL is", url);
+    console.log("requestType is", requestType);
+    console.log("contentType is", contentType);
+    console.log("data is", data);
+
+
+    //  if the request type is get, invoke fetch api to create a get request
+    if(requestType = "GET"){
+        fetch(url, {
+            method: 'GET',
+
+        })
+        .then(response => response.text())
+        .then((text) =>{
+            document.getElementById("responseJsonText").value = text;
+        })
+    }
+    // if the request type is POST/
+    else(
+        fetch(url, {
+            method: 'POT',
+            body : data,
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+              },
+
+        })
+        .then(response => response.text())
+        .then((text) =>{
+            document.getElementById("responseJsonText").value = text;
+        })
+    )
+
+
 })
 
